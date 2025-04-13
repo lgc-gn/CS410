ef669af3ae497e724da42ded01f4ca3991d922e4
wigglec0in <wigglecoin@icloud.com>
2025-04-12 19:56:55 -0700

    create sound trigger
    made a prefab to trigger the sound and made two versions of the same sound in case the first isnt loud enough - sorin
    
    so far my code for triggering the sound to play does play when entered but it doesnt check for if the player is walking, which would be the only way it makes sense considering i want it to be on a loop when in the squeaky hallway every time you move, so i might be making it more complicated than it needs to be for myself but i still want this to just be when the player is moving and not just when the player is there so theres more to do.

---
+++ Project 2/Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/soundTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//script to have the 'floarboards' in one particular hallway squeak when walked on
+//This Peice of the assignment was made by Sorin West, following the help of Chris' Turorials on youtube
+
+public class soundTrigger : MonoBehaviour
+{
+    AudioSource source;
+    Collider trigger;
+    public GameObject Player;
+
+    void Awake(){
+        source = GetComponent<AudioSource>();
+        trigger = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject == Player){
+            source.Play();
+        }
+    }
+
+
+}
